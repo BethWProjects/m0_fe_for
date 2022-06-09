be39a8abc34ef44e2df7eb6d9381a71ee0dc542f
Beth Wilson <bethwilson777@gmail.com>
2022-06-08 19:05:06 -0600

    For statement challenges

--- for.js
+++ for.js
@@ -10,13 +10,21 @@ for (var i = 24; i < 78; i+=2){
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
-weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
+weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"];
+for (var i = 0; i < weather_options.length; i++)
+{
+  console.log("There is a lot of weather one will see in Colorado, they could be:" + weather_options[i]);
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
-words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+for (var i = 0; i < words.length; i++)
+{
+  if (words[i].length === 3) {
+  console.log("List of words with three letters: " + words[i]);
+}  }
 // 📹 Record yourself live-coding and talking through any ONE of the challenges.
   // The recording doesn't have to be the first time you are solving the problem.
   // Use technical vocabulary and explain each part to demonstrate your understanding!
